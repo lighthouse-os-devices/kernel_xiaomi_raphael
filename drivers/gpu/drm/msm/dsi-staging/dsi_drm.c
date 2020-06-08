8c8c2c4e4f1049430417f552d52583d1dac0a722
Demon000 <demonsingur@gmail.com>
2020-06-08 17:18:11 +0200

    drm: msm: dsi-staging: Trigger drm notifier
    Change-Id: I225b36292f96eafb0703e3f0701f924f20b186d5

--- drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_drm.c
@@ -14,6 +14,8 @@
 
 
 #define pr_fmt(fmt)	"dsi-drm:[%s] " fmt, __func__
+#include <linux/msm_drm_notify.h>
+
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_atomic.h>
 #include <linux/notifier.h>
@@ -175,6 +177,8 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 {
 	int rc = 0;
 	struct dsi_bridge *c_bridge = to_dsi_bridge(bridge);
+	struct msm_drm_notifier notify_data;
+	int power_mode;
 	int event = MSM_DRM_BLANK_UNBLANK;
 	notify_data.data = &event;
 
@@ -191,6 +195,11 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 	msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
 
 	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
+
+	power_mode = sde_connector_get_lp(c_bridge->display->drm_conn);
+	notify_data.data = &power_mode;
+	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
+	msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
 
 	/* By this point mode should have been validated through mode_fixup */
 	rc = dsi_display_set_mode(c_bridge->display,
@@ -229,6 +238,8 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 
 	SDE_ATRACE_END("dsi_display_enable");
+
+	msm_drm_notifier_call_chain(MSM_DRM_EVENT_BLANK, &notify_data);
 
 	rc = dsi_display_splash_res_cleanup(c_bridge->display);
 	if (rc)
@@ -301,6 +312,8 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 {
 	int rc = 0;
 	struct dsi_bridge *c_bridge = to_dsi_bridge(bridge);
+	struct msm_drm_notifier notify_data;
+	int power_mode;
 	int event = MSM_DRM_BLANK_POWERDOWN;
 	notify_data.data = &event;
 
@@ -309,6 +322,9 @@ static void dsi_bridge_post_disable(struct drm_bridge *bridge)
 		return;
 	}
 
+	power_mode = sde_connector_get_lp(c_bridge->display->drm_conn);
+	notify_data.data = &power_mode;
+	notify_data.id = MSM_DRM_PRIMARY_DISPLAY;
 	msm_drm_notifier_call_chain(MSM_DRM_EARLY_EVENT_BLANK, &notify_data);
 
 	SDE_ATRACE_BEGIN("dsi_bridge_post_disable");
