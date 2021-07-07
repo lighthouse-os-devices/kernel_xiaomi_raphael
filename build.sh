ee91716715a8d3f9d8c8f297838a59de48ebcc20
CannedShroud <hypersync112@gmail.com>
2021-07-07 22:36:49 +0530

    raphael: Script revamp

--- build.sh
+++ build.sh
@@ -1,132 +1,312 @@
-#!/bin/bash
-rm .version
-# Bash Color
-green='\033[01;32m'
-red='\033[01;31m'
-blink_red='\033[05;31m'
-restore='\033[0m'
+#! /bin/bash
 
-clear
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
 
-# Resources
-export CLANG_PATH=/datadrive/syb/prebuilts/clang/host/linux-x86/clang-proton/bin
-export PATH=${CLANG_PATH}:${PATH}
-export CROSS_COMPILE=${CLANG_PATH}/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=${CLANG_PATH}/arm-linux-gnueabi-
-export THINLTO_CACHE=/datadrive/kernel/ltocache/
-DEFCONFIG="raphael_defconfig"
+#Kernel building script
 
-# Kernel Details
-VER="R1"
-
-# Paths
-KERNEL_DIR=`pwd`
-REPACK_DIR=/datadrive/AnyKernel3
-ZIP_MOVE=/datadrive/AK-releases
-
-# Functions
-function clean_all {
-		rm -rf $REPACK_DIR/Image*
-		cd $KERNEL_DIR
-		echo
-		make clean && make mrproper
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
 }
 
-function make_kernel {
-		echo
-		make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip $DEFCONFIG
-		make CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip -j$(grep -c ^processor /proc/cpuinfo)
-
+err() {
+    echo -e "\e[1;41m$*\e[0m"
 }
 
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
 
-function make_boot {
-		cp out/arch/arm64/boot/Image.gz-dtb $REPACK_DIR
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
+
+# The name of the device for which the kernel is built
+MODEL="THE GOD"
+
+# The codename of the device
+DEVICE="raphael"
+
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=raphael_defconfig
+
+# Show manufacturer info
+MANUFACTURERINFO="Xiaomeme"
+
+# Specify compiler.
+# 'clang' or 'clangxgcc' or 'gcc'
+COMPILER=gcc
+	if [ $COMPILER = "clang" ] || [ $COMPILER = "clangxgcc" ]
+	then
+		# install few necessary packages
+		sudo apt-get -y install gcc llvm lld g++-multilib clang
+	fi
+
+# Kernel is LTO
+LTO=1
+
+# Clean source prior building. 1 is NO(default) | 0 is YES
+INCREMENTAL=1
+
+# Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
+DEF_REG=0
+
+# Build dtbo.img (select this only if your source has support to building dtbo.img)
+# 1 is YES | 0 is NO(default)
+BUILD_DTBO=1
+
+# Sign the zipfile
+# 1 is YES | 0 is NO
+SIGN=0
+	if [ $SIGN = 1 ]
+	then
+		#Check for java
+		if command -v java > /dev/null 2>&1; then
+			SIGN=1
+		else
+			SIGN=0
+		fi
+	fi
+
+# Debug purpose. Send logs on every successfull builds
+# 1 is YES | 0 is NO(default)
+LOG_DEBUG=0
+
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
+
+# Check if we are using a dedicated CI ( Continuous Integration ), and
+# set KBUILD_BUILD_VERSION and KBUILD_BUILD_HOST and CI_BRANCH
+
+#Check Kernel Version
+LINUXVER=$(make kernelversion)
+
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
+
+# Set Date
+DATE=$(TZ=Asia/Kolkata date +"%Y-%m-%d")
+
+#Now Its time for other stuffs like cloning, exporting, etc
+
+ clone() {
+	echo " "
+	if [ $COMPILER = "clang" ]
+	then
+		msg "|| Cloning toolchain ||"
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+
+	elif [ $COMPILER = "gcc" ]
+	then
+		msg "|| Cloning GCC ||"
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1
+                git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		msg "|| Cloning toolchain ||"
+		git clone --depth=1 https://github.com/kdrag0n/proton-clang -b master clang
+
+		msg "|| Cloning GCC ||"
+		git clone https://github.com/mvaisakh/gcc-arm64.git gcc64 --depth=1
+		git clone https://github.com/mvaisakh/gcc-arm.git gcc32 --depth=1
+	fi
+
+	# Toolchain Directory defaults to clang-llvm
+		TC_DIR=$KERNEL_DIR/clang
+
+	# GCC Directory
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
+
+	msg "|| Cloning Anykernel ||"
+        git clone https://github.com/CannedShroud/AnyKernel3 -b raphael
+
+	if [ $BUILD_DTBO = 1 ]
+	then
+		msg "|| Cloning libufdt ||"
+		git clone https://android.googlesource.com/platform/system/libufdt "$KERNEL_DIR"/scripts/ufdt/libufdt
+	fi
 }
 
+##------------------------------------------------------##
 
-function make_zip {
-		cd $REPACK_DIR
-		zip -r9 `echo $ZIP_NAME`.zip *
-		mv  `echo $ZIP_NAME`*.zip $ZIP_MOVE
-		cd $KERNEL_DIR
+exports() {
+	export KBUILD_BUILD_USER="CannedShroud"
+	export ARCH=arm64
+	export SUBARCH=arm64
+
+	if [ $COMPILER = "clang" ]
+	then
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin/:$PATH
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+		PATH=$TC_DIR/bin:$GCC64_DIR/bin:$GCC32_DIR/bin:/usr/bin:$PATH
+	elif [ $COMPILER = "gcc" ]
+	then
+		KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+		PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+	fi
+
+	if [ $LTO = "1" ];then
+		export LD=ld.lld
+        export LD_LIBRARY_PATH=$TC_DIR/lib
+	fi
+
+	export PATH KBUILD_COMPILER_STRING
+	PROCS=$(nproc --all)
+	export PROCS
 }
 
+##----------------------------------------------------------##
 
-DATE_START=$(date +"%s")
+# Function to replace defconfig versioning
+setversioning() {
+    # For staging branch
+    KERNELNAME="Lighthouse-$LINUXVER-raphael-v0.1-$DATE"
+    # Export our new localversion and zipnames
+    export KERNELNAME
+    export ZIPNAME="$KERNELNAME.zip"
+}
+
+##--------------------------------------------------------------##
+
+build_kernel() {
+	if [ $INCREMENTAL = 0 ]
+	then
+		msg "|| Cleaning Sources ||"
+		make clean && make mrproper && rm -rf out
+	fi
+
+	msg "|| Started Compilation ||"
+
+	make O=out $DEFCONFIG
+	if [ $DEF_REG = 1 ]
+	then
+		cp .config arch/arm64/configs/$DEFCONFIG
+		git add arch/arm64/configs/$DEFCONFIG
+		git commit -m "$DEFCONFIG: Regenerate
+						This is an auto-generated commit"
+	fi
+
+	BUILD_START=$(date +"%s")
+
+	if [ $COMPILER = "clang" ]
+	then
+		make -j"$PROCS" O=out \
+				CROSS_COMPILE=aarch64-linux-gnu- \
+				CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+				CC=clang \
+				AR=llvm-ar \
+				OBJDUMP=llvm-objdump \
+				STRIP=llvm-strip
+	elif [ $COMPILER = "gcc" ]
+	then
+		make -j"$PROCS" O=out \
+				CROSS_COMPILE_ARM32=arm-eabi- \
+				CROSS_COMPILE=aarch64-elf- \
+				AR=aarch64-elf-ar \
+				OBJDUMP=aarch64-elf-objdump \
+				STRIP=aarch64-elf-strip
+	elif [ $COMPILER = "clangxgcc" ]
+	then
+		make -j"$PROCS"  O=out \
+					CC=clang \
+					CROSS_COMPILE=aarch64-linux-gnu- \
+					CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+					AR=llvm-ar \
+					AS=llvm-as \
+					NM=llvm-nm \
+					STRIP=llvm-strip \
+					OBJCOPY=llvm-objcopy \
+					OBJDUMP=llvm-objdump \
+					OBJSIZE=llvm-size \
+					READELF=llvm-readelf \
+					HOSTCC=clang \
+					HOSTCXX=clang++ \
+					HOSTAR=llvm-ar \
+					CLANG_TRIPLE=aarch64-linux-gnu-
+	fi
+
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+
+	if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
+	then
+		msg "|| Kernel successfully compiled ||"
+	elif ! [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ]
+	then
+		echo -e "Kernel compilation failed, See buildlog to fix errors"
+	fi
+
+	if [ $BUILD_DTBO = 1 ]
+	then
+		msg "|| Building DTBO ||"
+		python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+			create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/raphael-sm8150-overlay.dtbo"
+	fi
+}
+
+##--------------------------------------------------------------##
+
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+	if [ $BUILD_DTBO = 1 ]
+	then
+		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+	fi
+	cd AnyKernel3 || exit
+        cp -af anykernel-real.sh anykernel.sh
+	sed -i "s/kernel.string=.*/kernel.string=CannedShroud-R/g" anykernel.sh
+	sed -i "s/kernel.for=.*/kernel.for=P-WIFI/g" anykernel.sh
+	sed -i "s/kernel.compiler=.*/kernel.compiler=EVA-GCC/g" anykernel.sh
+	sed -i "s/kernel.made=.*/kernel.made=@CannedShroud/g" anykernel.sh
+	sed -i "s/kernel.version=.*/kernel.version=$LINUXVER/g" anykernel.sh
+	sed -i "s/message.word=.*/message.word=Appreciate your efforts for choosing CannedShroud kernel./g" anykernel.sh
+	sed -i "s/build.date=.*/build.date=$DATE/g" anykernel.sh
 
 
-echo -e "${green}"
-echo "-----------------"
-echo "Making Kernel:"
-echo "-----------------"
-echo -e "${restore}"
+	zip -r9 "$ZIPNAME" * -x .git README.md anykernel-real.sh .gitignore *.zip
+
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME"
 
 
-# Vars
-BASE_AK_VER="SOVIET-STAR-"
-DATE=`date +"%Y%m%d-%H%M"`
-AK_VER="$BASE_AK_VER$VER"
-ZIP_NAME="$AK_VER"-"$DATE"
-#export LOCALVERSION=~`echo $AK_VER`
-#export LOCALVERSION=~`echo $AK_VER`
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER=NATO66613
-export KBUILD_BUILD_HOST=KREMLIN
+	if [ $SIGN = 1 ]
+	then
+		## Sign the zip before sending it to telegram
+		if [ "$PTTG" = 1 ]
+ 		then
+ 			msg "|| Signing Zip ||"
+			tg_post_msg "<code>Signing Zip file with AOSP keys..</code>"
+ 		fi
+		java -jar zipsigner-3.0.jar "$KERNELNAME".zip "$KERNELNAME"-signed.zip
+		ZIP_FINAL="$ZIP_FINAL-signed"
+	fi
 
-echo
+	cd ..
+}
 
-while read -p "Do you want to clean stuffs (y/n)? " cchoice
-do
-case "$cchoice" in
-	y|Y )
-		clean_all
-		echo
-		echo "All Cleaned now."
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
+setversioning
+clone
+exports
+build_kernel
+gen_zip
 
-echo
-
-while read -p "Do you want to build?" dchoice
-do
-case "$dchoice" in
-	y|Y )
-		make_kernel
-		make_boot
-                make_zip
-		break
-		;;
-	n|N )
-		break
-		;;
-	* )
-		echo
-		echo "Invalid try again!"
-		echo
-		;;
-esac
-done
-
-
-echo -e "${green}"
-echo "-------------------"
-echo "Build Completed in:"
-echo "-------------------"
-echo -e "${restore}"
-
-DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
-echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-echo
+##------------------------------------------------------------------##
